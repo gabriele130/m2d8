feb03481a6f7a41f55e19315282a917406aeb63e
gabriele130 <114685382+gabriele130@users.noreply.github.com>
2022-11-21 19:52:48 +0100

    Add files via upload

---
+++ m2d8.js
@@ -0,0 +1,32 @@
+let x = "John"
+let y = "Doe "
+console.log( x + "<>" + y)
+
+
+const person = {
+    Name: "John",
+    SurName: "Doe",
+    Email: "johndoe@gmail.com"
+  };
+
+  delete person["Email"];
+  console.log( person)
+
+  let city = [
+    'dubai',
+    'tokyo',
+    'amsterdam',
+    'new york',
+    'istanbul',
+    'berlino',
+    'london',
+    'budapest',
+    'los angeles',
+    'paris'
+  ]
+  console.log(city)
+
+  let array = [
+    num = Math.round(Math.random()*100 + 1)
+  ]
+  console.log(array)
